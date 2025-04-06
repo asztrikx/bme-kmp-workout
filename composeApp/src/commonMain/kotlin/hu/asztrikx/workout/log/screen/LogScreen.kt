d1d654b57c76b86a77af81cdb3c88ce16b4fa66f
asztrikx <asztrik35@gmail.com>
2025-04-06 21:08:24 +0200

    update imports

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/log/screen/LogScreen.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/log/screen/LogScreen.kt
@@ -1,33 +1,24 @@
 package hu.asztrikx.workout.log.screen
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Timeline
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -39,7 +30,6 @@ import hu.asztrikx.workout.shared.BetterScaffold
 import hu.asztrikx.workout.shared.TopAppBarWithSettings
 import org.koin.compose.koinInject
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LogScreen(navHostController: NavHostController) {
 	val viewModel: LogViewModel = koinInject()
