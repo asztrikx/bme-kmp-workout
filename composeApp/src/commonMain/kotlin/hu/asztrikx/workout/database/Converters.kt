d29f74d02f84fb611b7f1de88f14bef1f9be648e
asztrikx <asztrik35@gmail.com>
2025-04-23 00:00:03 +0200

    fix ksp bug with @TypeConverter

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/database/Converters.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/database/Converters.kt
@@ -5,10 +5,22 @@ import kotlinx.datetime.LocalDate
 
 object Converters {
 	@TypeConverter
-	fun LocalDate.asString() =
+	fun asString(localDate: LocalDate) =
+		localDate.toString()
+
+	@TypeConverter
+	fun asLocalDate(string: String) =
+		LocalDate.parse(string)
+
+	/*
+	// KSP tries to use these as Converters.asString()
+	@TypeConverter
+	fun LocalDate.asString(): String =
 		toString()
 
 	@TypeConverter
-	fun String.asLocalDate() =
+	fun String.asLocalDate(): LocalDate =
 		LocalDate.parse(this)
+
+	*/
 }
