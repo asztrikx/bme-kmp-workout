44c6c9ccd5c0e40a67b25f8853674d2322a132de
asztrikx <asztrik35@gmail.com>
2025-04-28 22:35:22 +0200

    refactor di

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/App.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/App.kt
@@ -5,35 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.compose.rememberNavController
-import hu.asztrikx.workout.database.WorkoutDatabase
-import hu.asztrikx.workout.database.category.CategoryRepository
-import hu.asztrikx.workout.database.category.CategoryRepositoryDao
-import hu.asztrikx.workout.database.category.CategoryService
-import hu.asztrikx.workout.database.log.LogRepository
-import hu.asztrikx.workout.database.log.LogRepositoryDao
-import hu.asztrikx.workout.database.log.LogService
-import hu.asztrikx.workout.database.quantity.QuantityRepository
-import hu.asztrikx.workout.database.quantity.QuantityRepositoryDao
-import hu.asztrikx.workout.database.settings.SettingsRepository
-import hu.asztrikx.workout.database.settings.SettingsRepositoryDao
 import hu.asztrikx.workout.database.settings.SettingsService
-import hu.asztrikx.workout.database.stats.StatsRepository
-import hu.asztrikx.workout.database.stats.StatsRepositoryDao
-import hu.asztrikx.workout.database.stats.StatsService
-import hu.asztrikx.workout.presentation.log.LogViewModel
-import hu.asztrikx.workout.presentation.logEdit.LogEditViewModel
 import hu.asztrikx.workout.presentation.navigation.NavGraph
-import hu.asztrikx.workout.presentation.settings.SettingsViewModel
-import hu.asztrikx.workout.presentation.settings.categoryEdit.CategoryEditViewModel
-import hu.asztrikx.workout.presentation.settings.screen.SettingsScreenItemViewModel
-import hu.asztrikx.workout.presentation.stats.StatsViewModel
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.koinInject
-import org.koin.core.context.startKoin
-import org.koin.core.module.Module
-import org.koin.dsl.KoinAppDeclaration
-import org.koin.dsl.module
 
 @Composable
 @Preview
@@ -52,42 +28,5 @@ fun App() {
 
 	MaterialTheme {
 		NavGraph(navHostController)
-	}
-}
-
-expect fun platformModule(): Module
-
-val repositories = module {
-	includes(platformModule())
-	single<CategoryRepository> { CategoryRepositoryDao(get<WorkoutDatabase>().categoryDao) }
-	single<LogRepository> { LogRepositoryDao(get<WorkoutDatabase>().logDao) }
-	single<QuantityRepository> { QuantityRepositoryDao(get<WorkoutDatabase>().quantityDao) }
-	single<SettingsRepository> { SettingsRepositoryDao(get<WorkoutDatabase>().settingsDao) }
-	single<StatsRepository> { StatsRepositoryDao(get<WorkoutDatabase>().statsDao) }
-}
-
-val services = module {
-	includes(repositories)
-	single { CategoryService(get()) }
-	single { LogService(get()) }
-	//single { QuantityService(get<QuantityRepository>()) }
-	single { SettingsService(get()) }
-	single { StatsService(get()) }
-}
-
-val viewModels = module {
-	includes(services)
-	single { CategoryEditViewModel(get()) }
-	single { LogViewModel(get()) }
-	single { LogEditViewModel(get(), get()) }
-	single { SettingsViewModel(get()) }
-	single { StatsViewModel(get()) }
-	single { SettingsScreenItemViewModel(get()) }
-}
-
-fun initKoin(config: KoinAppDeclaration? = null) {
-	startKoin {
-		config?.invoke(this)
-		modules(viewModels)
 	}
 }
---
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/DI.kt
@@ -0,0 +1,64 @@
+package hu.asztrikx.workout
+
+import hu.asztrikx.workout.database.WorkoutDatabase
+import hu.asztrikx.workout.database.category.CategoryRepository
+import hu.asztrikx.workout.database.category.CategoryRepositoryDao
+import hu.asztrikx.workout.database.category.CategoryService
+import hu.asztrikx.workout.database.log.LogRepository
+import hu.asztrikx.workout.database.log.LogRepositoryDao
+import hu.asztrikx.workout.database.log.LogService
+import hu.asztrikx.workout.database.quantity.QuantityRepository
+import hu.asztrikx.workout.database.quantity.QuantityRepositoryDao
+import hu.asztrikx.workout.database.settings.SettingsRepository
+import hu.asztrikx.workout.database.settings.SettingsRepositoryDao
+import hu.asztrikx.workout.database.settings.SettingsService
+import hu.asztrikx.workout.database.stats.StatsRepository
+import hu.asztrikx.workout.database.stats.StatsRepositoryDao
+import hu.asztrikx.workout.database.stats.StatsService
+import hu.asztrikx.workout.presentation.log.LogViewModel
+import hu.asztrikx.workout.presentation.logEdit.LogEditViewModel
+import hu.asztrikx.workout.presentation.settings.SettingsViewModel
+import hu.asztrikx.workout.presentation.settings.categoryEdit.CategoryEditViewModel
+import hu.asztrikx.workout.presentation.settings.screen.SettingsScreenItemViewModel
+import hu.asztrikx.workout.presentation.stats.StatsViewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+expect fun platformModule(): Module
+
+val repositories = module {
+	includes(platformModule())
+	single<CategoryRepository> { CategoryRepositoryDao(get<WorkoutDatabase>().categoryDao) }
+	single<LogRepository> { LogRepositoryDao(get<WorkoutDatabase>().logDao) }
+	single<QuantityRepository> { QuantityRepositoryDao(get<WorkoutDatabase>().quantityDao) }
+	single<SettingsRepository> { SettingsRepositoryDao(get<WorkoutDatabase>().settingsDao) }
+	single<StatsRepository> { StatsRepositoryDao(get<WorkoutDatabase>().statsDao) }
+}
+
+val services = module {
+	includes(repositories)
+	single { CategoryService(get()) }
+	single { LogService(get()) }
+	//single { QuantityService(get<QuantityRepository>()) }
+	single { SettingsService(get()) }
+	single { StatsService(get()) }
+}
+
+val viewModels = module {
+	includes(services)
+	single { CategoryEditViewModel(get()) }
+	single { LogViewModel(get()) }
+	single { LogEditViewModel(get(), get()) }
+	single { SettingsViewModel(get()) }
+	single { StatsViewModel(get()) }
+	single { SettingsScreenItemViewModel(get()) }
+}
+
+fun initKoin(config: KoinAppDeclaration? = null) {
+	startKoin {
+		config?.invoke(this)
+		modules(viewModels)
+	}
+}
