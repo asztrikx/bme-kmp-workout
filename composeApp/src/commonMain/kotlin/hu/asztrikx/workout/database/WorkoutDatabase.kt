22f459758d0030c30d06d4a895dd6d9f41e0f4f3
asztrikx <asztrik35@gmail.com>
2025-04-29 14:31:54 +0200

    refactor database

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/database/WorkoutDatabase.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/database/WorkoutDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import hu.asztrikx.workout.database.category.CategoryDao
 import hu.asztrikx.workout.database.category.CategoryEntity
 import hu.asztrikx.workout.database.log.LogDao
@@ -14,6 +15,7 @@ import hu.asztrikx.workout.database.quantity.QuantityEntity
 import hu.asztrikx.workout.database.settings.SettingsDao
 import hu.asztrikx.workout.database.settings.SettingsEntity
 import hu.asztrikx.workout.database.stats.StatsDao
+import kotlinx.coroutines.Dispatchers
 
 @Database(
 	entities = [
@@ -22,7 +24,7 @@ import hu.asztrikx.workout.database.stats.StatsDao
 		QuantityEntity::class,
 		SettingsEntity::class,
 	],
-	version = 4
+	version = 5
 )
 @ConstructedBy(AppDatabaseConstructor::class)
 @TypeConverters(Converters::class)
@@ -37,4 +39,16 @@ abstract class WorkoutDatabase : RoomDatabase() {
 @Suppress("NO_ACTUAL_FOR_EXPECT", "EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<WorkoutDatabase> {
 	override fun initialize(): WorkoutDatabase
+}
+
+fun getRoomDatabase(
+	builder: RoomDatabase.Builder<WorkoutDatabase>
+): WorkoutDatabase {
+	return builder
+		.addMigrations()
+		.fallbackToDestructiveMigrationOnDowngrade(true)
+		.fallbackToDestructiveMigration(true)
+		.setDriver(BundledSQLiteDriver())
+		.setQueryCoroutineContext(Dispatchers.IO)
+		.build()
 }
