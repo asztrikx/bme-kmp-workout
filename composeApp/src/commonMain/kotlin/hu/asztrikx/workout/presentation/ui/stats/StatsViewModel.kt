dfd34221dfde8914cc7258fe13c38a2a3848db6f
asztrikx <asztrik35@gmail.com>
2025-04-29 15:23:31 +0200

    add settings date as default value

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/presentation/ui/stats/StatsViewModel.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/presentation/ui/stats/StatsViewModel.kt
@@ -4,13 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import hu.asztrikx.workout.presentation.mapper.asUI
 import hu.asztrikx.workout.service.stats.StatsService
-import hu.asztrikx.workout.presentation.ui.shared.currentDate
+import hu.asztrikx.workout.service.settings.SettingsService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class StatsViewModel(private val service: StatsService): ViewModel() {
+class StatsViewModel(
+	private val service: StatsService,
+	private val settingsService: SettingsService,
+): ViewModel() {
 	private val _state = MutableStateFlow<StatsState>(StatsState.Loading)
 	val state = _state.asStateFlow()
 
@@ -22,13 +26,16 @@ class StatsViewModel(private val service: StatsService): ViewModel() {
 		viewModelScope.launch {
 			try {
 				_state.update { StatsState.Loading }
-				service.getAll().collect { statsByCategory ->
-					_state.update {
-						StatsState.Result(
-							statsByCategory.map { it.asUI() },
-							currentDate() // TODO from settings
-						)
-					}
+				combine(
+					settingsService.getWithCategories(),
+					service.getAll(),
+				) { settings, statsByCategory ->
+					StatsState.Result(
+						statsByCategory.map { it.asUI() },
+						settings!!.startDate,
+					)
+				}.collect { result ->
+					_state.update { result }
 				}
 			} catch (e: Exception) {
 				_state.update { StatsState.Error(e) }
