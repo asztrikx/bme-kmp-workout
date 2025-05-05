448ee4128a94b1b31331bbda851962b9e6fe5703
asztrikx <asztrik35@gmail.com>
2025-05-05 22:12:52 +0200

    fix LogEdit receiving "add" ad {id} only on desktop

--- composeApp/src/commonMain/kotlin/hu/asztrikx/workout/presentation/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/hu/asztrikx/workout/presentation/navigation/Screen.kt
@@ -2,7 +2,7 @@ package hu.asztrikx.workout.presentation.navigation
 
 sealed class Screen(val route: String) {
 	data object Log: Screen("log")
-	data object LogAdd: Screen("log/add")
+	data object LogAdd: Screen("logAdd")
 	data object LogEdit: Screen("log/{id}") {
 		fun createRoute(id: Long) = "log/$id"
 	}
