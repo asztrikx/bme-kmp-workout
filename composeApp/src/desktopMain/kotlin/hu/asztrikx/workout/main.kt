47cb83f9d2a97734831078b3d1c0b853b77353e9
asztrikx <asztrik35@gmail.com>
2025-05-06 14:51:08 +0200

    add icon

--- composeApp/src/desktopMain/kotlin/hu/asztrikx/workout/main.kt
+++ composeApp/src/desktopMain/kotlin/hu/asztrikx/workout/main.kt
@@ -2,12 +2,16 @@ package hu.asztrikx.workout
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.painterResource
+import workout.composeapp.generated.resources.Res
+import workout.composeapp.generated.resources.play_store_512
 
 fun main() = application {
 	initKoin()
 	Window(
 		onCloseRequest = ::exitApplication,
 		title = "Workout",
+		icon = painterResource(Res.drawable.play_store_512),
 	) {
 		App()
 	}
